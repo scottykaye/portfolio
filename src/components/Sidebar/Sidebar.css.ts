09084879c42dcac2650650db3059ba6d63ec00ab
Scott Kaye <skaye@wayfair.com>
2022-08-30 10:17:55 -0400

    Basic Sidebar start

--- src/components/Sidebar/Sidebar.css.ts
+++ src/components/Sidebar/Sidebar.css.ts
@@ -1,6 +1,27 @@
-import { style } from '@vanilla-extract/css';
+import {
+  style, createVar,
+} from '@vanilla-extract/css';
 // import { theme } from '../../theme/theme.css';
 
-export const app = style({
-  display: 'block',
+export const sidebarSize = createVar();
+
+export const wrapper = style({
+  display: 'flex',
+  minHeight: '100%',
+});
+
+export const page = style({
+  flexGrow: '1',
+  transition: 'flex-basis .2s ease-in',
+  overflow: 'auto',
+
+});
+
+export const sidebar = style({
+  flexBasis: sidebarSize,
+  flexGrow: '1',
+  flexShrink: '0',
+  transition: 'flex-basis .2s ease-in',
+  overflow: 'auto',
+  scrollBehavior: 'smooth',
 });
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,3 +1,37 @@
-export default function Sidebar() {
-  return 'Sidebar';
+import React from 'react';
+import { assignInlineVars } from '@vanilla-extract/dynamic';
+import * as styles from './Sidebar.css';
+
+function Wrapper(props) {
+  return <div className={styles.wrapper}>{props.children}</div>;
 }
+
+function Page(props) {
+  return <div className={styles.page}>{props.children}</div>;
+}
+
+interface Props {
+  children: React.ReactNode;
+  resize?: string;
+  ariaLabel?: string;
+  props: { [key: string]: unknown };
+}
+
+export default function Sidebar({
+  children, resize = '32px', ariaLabel = 'Navigation', ...props
+}: Props) {
+  return (
+    <nav
+      className={styles.sidebar}
+      style={assignInlineVars({ [styles.sidebarSize]: resize })}
+      aria-label={ariaLabel}
+      {...props}
+
+    >
+      {children}
+    </nav>
+  );
+}
+
+Sidebar.Wrapper = Wrapper;
+Sidebar.Page = Page;
