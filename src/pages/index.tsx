9a39f4fa222b69a9ac7c2004717318e2ce1f851e
Scott Kaye <skaye@wayfair.com>
2024-01-05 15:37:39 -0500

    Fix this stuff

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,44 @@
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
-import Link from "../components/Link";
-import Heading from "../components/Heading";
-import Card from "../components/Card";
-import Article from "../components/Article";
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Link from '../components/Link'
+import Heading from '../components/Heading'
+import Card from '../components/Card'
+import Article from '../components/Article'
 
-const blogDirectory = path.join(process.cwd(), "src/blog");
+const blogDirectory = path.join(process.cwd(), 'src/blog')
 
 function getSortedPostsData() {
-  const fileNames = fs.readdirSync(blogDirectory);
+  const fileNames = fs.readdirSync(blogDirectory)
   const allPostsData = fileNames.map((fileName) => {
     // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.mdx$/, "");
+    const id = fileName.replace(/\.mdx$/, '')
 
     // Read markdown file as string
-    const fullPath = path.join(blogDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, "utf8");
+    const fullPath = path.join(blogDirectory, fileName)
+    const fileContents = fs.readFileSync(fullPath, 'utf8')
     // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
+    const matterResult = matter(fileContents)
     // Combine the data with the id
     return {
       id,
       ...matterResult.data,
-    };
-  });
+    }
+  })
 
   return allPostsData.sort((a, b) => {
     if (a.date < b.date) {
-      return 1;
+      return 1
     } else {
-      return -1;
+      return -1
     }
-  });
+  })
 }
 
 export async function getServerSideProps() {
-  const posts = getSortedPostsData();
+  const posts = getSortedPostsData()
 
-  return { props: { posts } };
+  return { props: { posts } }
 }
 
 export default function Home(props) {
@@ -46,10 +46,10 @@ export default function Home(props) {
     <>
       <header>
         <Heading>{`Hi, I'm Scott`} &#128075;</Heading>
-        {`I'm a Frontend Software Engineer at`}{" "}
+        {`I'm a Frontend Software Engineer at`}{' '}
         <Link href="http://wayfair.com/" target="_blank" rel="noreferrer">
           Wayfair
-        </Link>{" "}
+        </Link>{' '}
         where I work on the Frontend Platform Team.
       </header>
       {props.posts.map((post) => {
@@ -60,8 +60,8 @@ export default function Home(props) {
               {post.date}
             </Article>
           </Card>
-        );
+        )
       })}
     </>
-  );
+  )
 }
