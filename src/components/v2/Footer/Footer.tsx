d8fee2fb70f6cce6076f3363ba7c55658ea57aed
Scott Kaye <skaye@wayfair.com>
2024-08-27 07:35:06 -0400

    Updates

--- src/components/v2/Footer/Footer.tsx
+++ src/components/v2/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react'
 export function Footer({ children }: { children: ReactNode }) {
   return (
     <footer className="border-t border-border backdrop-hue-rotate-30 print:hidden mt-48">
-      <ul className="py-10 px-5 max-w-3xl mx-auto flex justify-between items-center gap-2">
+      <ul className="[&:has(:is(:hover,:focus))_a]:opacity-50 [&_a:is(:hover,:focus)]:opacity-100 py-10 px-5 max-w-3xl mx-auto flex justify-between items-center gap-2">
         {children}
       </ul>
     </footer>
--- src/components/v2/Header/Header.tsx
+++ src/components/v2/Header/Header.tsx
@@ -19,7 +19,7 @@ export function Header({
   return (
     <header
       className={clsx(
-        'bg-background/50 dark:bg-background/30 fixed inset-x-0 backdrop-blur-sm backdrop-hue-rotate-30 border-b border-border p-5 z-40',
+        'Header bg-background/50 dark:bg-background/30 fixed inset-x-0 backdrop-blur-sm backdrop-hue-rotate-30 border-b border-border p-5 z-40',
         className,
       )}
     >
