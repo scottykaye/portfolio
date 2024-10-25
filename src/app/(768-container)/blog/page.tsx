d70b94b4464090cd1bc35d6dee6f916452058682
ScottyKaye <scott.kaye@tollbit.com>
2024-10-25 16:43:48 -0400

    add blog post

--- src/app/(768-container)/blog/page.tsx
+++ src/app/(768-container)/blog/page.tsx
@@ -4,21 +4,15 @@ import matter from 'gray-matter'
 
 import Link from 'next/link'
 export default function Blog() {
-  // 1) Set blogs directory
   const blogDir = 'src/blog'
 
-  // 2) Find all files in the blog directory
   const files = fs.readdirSync(path.join(blogDir))
 
-  // 3) For each blog found
   const blogs = files.map((filename) => {
-    // 4) Read the content of that blog
     const fileContent = fs.readFileSync(path.join(blogDir, filename), 'utf-8')
 
-    // 5) Extract the metadata from the blog's content
     const { data: frontMatter } = matter(fileContent)
 
-    // 6) Return the metadata and page slug
     return {
       meta: frontMatter,
       slug: filename.replace('.mdx', ''),
@@ -32,7 +26,7 @@ export default function Blog() {
         {blogs.map((blog) => (
           <>
             <Link
-              className="block group [&:not(:first-child)]:border-border [&:not(:first-child)]:border-t"
+              className="block group [&:not(:last-child)]:border-border [&:not(:last-child)]:border-b"
               href={'/blog/' + blog.slug}
               key={blog.slug}
             >
