497c1fb3b1d371daf9aca3e159e8fa884ea550fc
ScottyKaye <scott.kaye@tollbit.com>
2024-10-29 11:13:54 -0400

    Why Tailwind Blogpost

--- src/app/(1280-container)/layout.tsx
+++ src/app/(1280-container)/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 export default function ContainerLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="Scroll grid mt-28 max-w-7xl mx-auto p-5 print:mt-0">
+    <div className="Scroll grid grid-cols-1 mt-28 max-w-7xl mx-auto p-5 print:mt-0">
       {children}
     </div>
   )
