58e9f95f14b8a40e4b55b17ee4ab166562c1ba94
Matt Hamlin <matthewjameshamlin@gmail.com>
2024-10-30 09:30:21 -0400

    Comment out focus stuff for noww

--- src/app/(768-container)/blog/[slug]/page.tsx
+++ src/app/(768-container)/blog/[slug]/page.tsx
@@ -5,7 +5,10 @@ import Heading from '@components/v2/Heading'
 import Link from '@root/src/components/Link'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import Image from 'next/image'
-import { Code, type Extension } from 'bright'
+import { 
+  Code,
+  //type Extension
+} from 'bright'
 
 Code.theme = {
   dark: 'github-dark',
@@ -14,76 +17,76 @@ Code.theme = {
   darkSelector: 'html.dark',
 }
 
-const focus: Extension = {
-  name: "focus",
-  MultilineAnnotation: ({ children }) => (
-    <div style={{ filter: "contrast(0.1)" }}>{children}</div>
-  ),
-  beforeHighlight: (props, focusAnnotations) => {
-    if (focusAnnotations.length === 0) return props
+// const focus: Extension = {
+//   name: "focus",
+//   MultilineAnnotation: ({ children }) => (
+//     <div style={{ filter: "contrast(0.1)" }}>{children}</div>
+//   ),
+//   beforeHighlight: (props, focusAnnotations) => {
+//     if (focusAnnotations.length === 0) return props
 
-    const lineCount = props.code.split("\n").length
+//     const lineCount = props.code.split("\n").length
 
-    const ranges = focusAnnotations.flatMap((a) => a.ranges)
+//     const ranges = focusAnnotations.flatMap((a) => a.ranges)
 
-    let newRanges = [{ fromLineNumber: 1, toLineNumber: lineCount }]
+//     let newRanges = [{ fromLineNumber: 1, toLineNumber: lineCount }]
 
-    for (const range of ranges) {
-      const { fromLineNumber, toLineNumber } = range
-      newRanges = newRanges.flatMap((r) => {
-        if (
-          r.fromLineNumber > toLineNumber ||
-          r.toLineNumber < fromLineNumber
-        )
-          return [r]
-        if (
-          r.fromLineNumber >= fromLineNumber &&
-          r.toLineNumber <= toLineNumber
-        )
-          return []
-        if (
-          r.fromLineNumber < fromLineNumber &&
-          r.toLineNumber > toLineNumber
-        )
-          return [
-            {
-              fromLineNumber: r.fromLineNumber,
-              toLineNumber: fromLineNumber - 1,
-            },
-            {
-              fromLineNumber: toLineNumber + 1,
-              toLineNumber: r.toLineNumber,
-            },
-          ]
-        if (r.fromLineNumber < fromLineNumber)
-          return [
-            {
-              fromLineNumber: r.fromLineNumber,
-              toLineNumber: fromLineNumber - 1,
-            },
-          ]
-        if (r.toLineNumber > toLineNumber)
-          return [
-            {
-              fromLineNumber: toLineNumber + 1,
-              toLineNumber: r.toLineNumber,
-            },
-          ]
-      })
-    }
+//     for (const range of ranges) {
+//       const { fromLineNumber, toLineNumber } = range
+//       newRanges = newRanges.flatMap((r) => {
+//         if (
+//           r.fromLineNumber > toLineNumber ||
+//           r.toLineNumber < fromLineNumber
+//         )
+//           return [r]
+//         if (
+//           r.fromLineNumber >= fromLineNumber &&
+//           r.toLineNumber <= toLineNumber
+//         )
+//           return []
+//         if (
+//           r.fromLineNumber < fromLineNumber &&
+//           r.toLineNumber > toLineNumber
+//         )
+//           return [
+//             {
+//               fromLineNumber: r.fromLineNumber,
+//               toLineNumber: fromLineNumber - 1,
+//             },
+//             {
+//               fromLineNumber: toLineNumber + 1,
+//               toLineNumber: r.toLineNumber,
+//             },
+//           ]
+//         if (r.fromLineNumber < fromLineNumber)
+//           return [
+//             {
+//               fromLineNumber: r.fromLineNumber,
+//               toLineNumber: fromLineNumber - 1,
+//             },
+//           ]
+//         if (r.toLineNumber > toLineNumber)
+//           return [
+//             {
+//               fromLineNumber: toLineNumber + 1,
+//               toLineNumber: r.toLineNumber,
+//             },
+//           ]
+//       })
+//     }
 
-    const newAnnotations = props.annotations.filter(
-      (a) => a.name !== "focus"
-    )
-    newAnnotations.push({
-      name: "focus",
-      ranges: newRanges,
-    })
-    return { ...props, annotations: newAnnotations }
-  },
-}
+//     const newAnnotations = props.annotations.filter(
+//       (a) => a.name !== "focus"
+//     )
+//     newAnnotations.push({
+//       name: "focus",
+//       ranges: newRanges,
+//     })
+//     return { ...props, annotations: newAnnotations }
+//   },
+// }
 
-Code.extensions = [focus]
+// Code.extensions = [focus]
 
 export async function generateMetadata({
   params,
